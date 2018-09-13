9309d5f93290402648b9ffc7a2906b6e3fbd844c
cauldyclark15 <jcing.jc@gmail.com>
2018-09-13 10:17:36 +0800

    Changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">DOCKER TRAVIS INTEGRATED</h1>
+          <h1 className="App-title">NEW GCP PROJECT</h1>
         </header>
         <p className="App-intro">WE CAN DO THIS!</p>
       </div>
